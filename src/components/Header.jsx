69ffafebffd9550355bc4b0cb136ec775409f4f2
AkshithaRao123 <akshashw@gmail.com>
2025-05-10 13:37:03 +0530

    sign up frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BACKGROUND_IMAGE } from '../utils/constants'
 import { NETFLIX_LOGO_URL } from '../utils/constants'
 
 const Header = () => {
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,17 +3,27 @@ import Header from './Header'
 import { BACKGROUND_IMAGE } from '../utils/constants'
 
 const Login = () => {
+
+  const [isSignInForm, setIsSignInForm] = React.useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  }
+
   return (
     <div className='absolute top-0 z-1'>
       <Header />
       <div className='center-screen'>
-      <img className='contrast-50' src={BACKGROUND_IMAGE} alt="background" />
-      <form className='fixed bg-black z-10 login-form text-white'>
-        <label className='font-bold text-2xl pb-5'>Sign in</label>
-        <input type="text" placeholder="Username" className='p-2 m-2 outline-1 outline-white rounded-md bg-gray-800' />
-        <input type="text" placeholder="Password" className='p-2 m-2 outline-1 outline-white rounded-md bg-gray-800' />
-        <button type="submit" className='p-4 m-4'>Login</button>
-      </form>
+        <img className='contrast-50' src={BACKGROUND_IMAGE} alt="background" />
+        <form className='fixed bg-black z-10 login-form text-white'>
+          <label className='font-bold text-2xl pb-5'>{isSignInForm ? "Sign In" : "Sign Up"}</label>
+          {!isSignInForm && <input type="text" placeholder="Full Name" className='p-2 m-2 outline-1 outline-white rounded-md bg-gray-800' />}
+          <input type="text" placeholder="Email ID" className='p-2 m-2 outline-1 outline-white rounded-md bg-gray-800' />
+          <input type="text" placeholder="Password" className='p-2 m-2 outline-1 outline-white rounded-md bg-gray-800' />
+          {!isSignInForm && <input type="text" placeholder="Confirm Password" className='p-2 m-2 outline-1 outline-white rounded-md bg-gray-800' />}
+          <button type="submit" className='p-4 m-2'>{isSignInForm ? "Sign In": "Sign Up"}</button>
+          <p className='text-sm py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign up now!" : "Already a user? Login."}</p>
+        </form>
       </div>
     </div>
   )
