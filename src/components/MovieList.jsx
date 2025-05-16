964f619ea08b02cb3720af76f71e195112ed24a0
AkshithaRao123 <akshashw@gmail.com>
2025-05-16 10:57:39 +0530

    redux issue while reloading solved

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,12 +3,22 @@ import MovieCard from "./MovieCard"
 const MovieList = ({ title, movies }) => {
     return (
         <div>
-            <h1>{title}</h1>
+            <h2>{title}</h2>
             <div>
-                <p>{movies[0].poster_path}</p>
+                {/* <p>{movies[0].poster_path}</p>
                 {movies.map((movie) => {
                     <MovieCard posterPath={movie.poster_path} />
-                })}
+                })} */}
+                {
+                    (movies
+                        ?
+                        movies.map((movie) => {
+                            <MovieCard posterPath={movie.poster_path} />
+                        })
+                        :
+                        <p>No movies available</p>
+                    )
+                }
             </div>
         </div>
     )
